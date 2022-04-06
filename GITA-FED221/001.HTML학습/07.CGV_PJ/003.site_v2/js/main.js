63843b5e2a3d6c26c43c54c686ff1134806f4f43
최은재 <cycejc@gmail.com>
2022-04-06 18:46:17 +0900

    .

--- GITA-FED221/001.HTML학습/07.CGV_PJ/003.site_v2/js/main.js
+++ GITA-FED221/001.HTML학습/07.CGV_PJ/003.site_v2/js/main.js
@@ -2,7 +2,7 @@
 
 /*************************************** 
     함수명: chgMV
-    기능: 영화예고편을 변경함
+    기능: 유튜브 영상을 변경함
 ***************************************/
 function chgMV(mvid){ // mvid - 영화아이디값 전달변수
 
@@ -13,7 +13,7 @@ function chgMV(mvid){ // mvid - 영화아이디값 전달변수
     var tg = document.querySelector("#screen iframe");
 
     // 3. 변경내용: 대상의 src속성 변경하기
-    // src중 영화 아이디부분을 변경함!
+    // src중 유튜브 아이디부분을 변경함!
     tg.src = `https://www.youtube.com/embed/${mvid}?autoplay=1&playsinline=1`;
 
 
